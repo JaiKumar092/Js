ca454c39995c708d258bde19b7c0127a30295963
JaiKumar092 <142215489+JaiKumar092@users.noreply.github.com>
2023-12-14 18:50:41 +0000

    datatypes

--- set1/first.js
+++ set1/first.js
@@ -1,1 +1,22 @@
-console.log("Jai KUmar")
+console.log("Jai Kumar")
+
+const accountId=12345
+// accountId=12346
+console.log(accountId)
+accountName="Jai"
+console.log(accountName)
+
+
+// Datatypes 
+// numner=> 2 to power 53
+//bigint
+// string=> ""
+//boolean=> true/false
+//null=> standalone value
+//undefined=> 
+//symbol
+//object
+
+console.log(typeof("Jai Kumar"))
+console.log(typeof(null))
+console.log(typeof(undefined))

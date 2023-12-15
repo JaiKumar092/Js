14856bdd168cce98e7d98113a42d1276f4127e92
JaiKumar092 <142215489+JaiKumar092@users.noreply.github.com>
2023-12-15 09:45:08 +0000

    first day till  strings

--- set1/first.js
+++ set1/first.js
@@ -1,10 +1,11 @@
-console.log("Jai Kumar")
-
+// console.log("Jai Kumar")
+/*
 const accountId=12345
 // accountId=12346
 console.log(accountId)
 accountName="Jai"
 console.log(accountName)
+*/
 
 
 // Datatypes 
@@ -17,6 +18,93 @@ console.log(accountName)
 //symbol
 //object
 
+/*
 console.log(typeof("Jai Kumar"))
 console.log(typeof(null))
 console.log(typeof(undefined))
+*/
+
+//Conversion
+/*
+let score=33
+console.log(typeof(score))
+let age="18abc"
+let valueinnumber=Number(score)
+console.log(typeof valueinnumber)
+console.log(valueinnumber)
+*/
+//if variable is not able to convert into number it transforms itself into Nan but its type remains into number
+
+//null=> 0 true=>1 false=>0
+//"33"=>33
+//"33abc"=>Nan
+
+//1=>true 0=>false
+//""=>false
+//"hitesh"=> true
+
+//by using String(number) we can reverse the conversion from integer to string
+
+
+// //Operations
+// let value=3
+// let negvalue=-value
+// console.log(negvalue)
+
+// /used for division %used for modulo
+
+// console.log("1"+2)//=> 12 left to right addition with state in mind of converting interger to string and then addition
+// console.log(1+"2")//=> 12
+// console.log("1"+2+2)//=> 122
+// console.log(1+2+"2")//=> 32
+
+//if string is first then all are converted into strings
+//and first number then add as intergers and then converted into strings
+
+// console.log(+true)
+// console.log(+"")
+
+//  console.log("2">1)
+//  console.log("02">1)
+/*
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+
+//comparison operator convert the datatype accordingly
+
+//strict check ===
+console.log("2"==2)//=>true
+console.log("2"===2)//=>false
+*/
+
+//Primitive Data types
+//7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+//Reference types(Non Primitive types)
+//Array, Objects, Functions
+
+//symbol
+/*
+const id=Symbol('123')
+const anotherid=Symbol('123')
+console.log(id==anotherid)
+*/
+
+//typeof tells about the datatype of the variable
+//null=>object
+//object=>function
+//function=>object function
+
+//stack and heap
+//stack(Primitive) and heap(Non-Primitive)
+//stack creates copy of the object while heap creates the pointer to the same memory location
+
+
+//string can be denoted by single or double quote
+// to concatenate use
+//either use (`Hello my name is ${name} and my repocount is ${repocount}`)
+//access ,gamename.__proto__
+//trim,replace,includes,splitk
+
+//numers and maths
+//

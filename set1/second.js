9383e2c74b616a6fac62b31e31332a3951e3ee78
JaiKumar092 <142215489+JaiKumar092@users.noreply.github.com>
2023-12-15 10:30:57 +0000

    numbers

---
+++ set1/second.js
@@ -0,0 +1,35 @@
+//numbers
+//implicit data type const score=400
+//explicit data type 
+
+/*
+const balance=new Number(100)
+console.log(balance)
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(1))
+
+const othernumber=23.8966 //123.8966=>124  1123=>1.12e+3
+
+console.log(othernumber.toPrecision(3))
+
+const hundreds=1000000
+console.log(hundreds.toLocaleString('en-In'))
+*/
+
+//Max integer and min integer in javascript  NUmber.MAX_VALUE
+
+// Maths
+//console.log(Math);
+//absolute value  console.log(math.abs(-4))
+//round, ceil, floor
+//min max
+//random function
+console.log(Math.random()) //gives values between one and three
+console.log(Math.random()*10)
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
